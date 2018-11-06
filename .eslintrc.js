0c27c71c1ab769d8ebb5a80a61b9d10c374ef10c
OlafurjonHI <ojv1@hi.is>
2018-11-06 14:33:57 +0000

    Mögulega búinn þarf kannski að fínpússa nokkur atriði

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    env: {
+    	browser: true
+  },
+    rules: {
+    	indent: ['error', 2],
+    	curly: ['error', 'multi-line'],
+    	'brace-style': ['error'],
+  }
+};
