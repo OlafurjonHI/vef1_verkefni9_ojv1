ab3cae95e95d4057551cfe0d1d17a96b14729ee9
OlafurjonHI <ojv1@hi.is>
2018-11-06 15:11:14 +0000

    losað við óþarfa kóða, ásamt full esLintað

--- scripts.js
+++ scripts.js
@@ -5,110 +5,98 @@ const domains = document.querySelector('.domains');
  * Leit að lénum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  let form = document.querySelector('form');
   const results = domains.querySelector('.results');
 
-  function fetcher(dom) {
-  	let time = new Date()
-  	loading();
-    fetch(`${API_URL}${dom}`)
-  	  .then((result) => {
-        if (!result.ok){
-		  displayErrorMessage('Villa í tengingu');
-		  return;
-		}
-			console.log(time - (new Date()));
-			return result.json();
-  	  })
-  		.then((data)=> {
-  			displayResults(data.results);
-  		})
-  		.catch(error => console.error("error: "+error));
-  }
-
-  function loading(state){
-  	while(state === undefined){
-  		console.log("HELP");
-  	}
-  	console.log("done");
-  }
-
-  function formHandler(e) {
-  	e.preventDefault();
-  	const domain = document.querySelector("input").value;
-  	if (domain.trim() == ""){
-  		displayErrorMessage("Lén verður að vera strengur");
-  		document.querySelector("input").value = "";
-  		return;
-  	}
-  	fetcher(domain);
+  function emptyResults() {
+    while (results.firstChild) {
+      results.removeChild(results.firstChild);
+    }
   }
 
   function displayErrorMessage(message) {
-  	emptyResults();
-  	let span = document.createElement("span");
-  	span.appendChild(document.createTextNode(message));
-  	results.appendChild(span);
-  }
-  function emptyResults(){
-  	while(results.firstChild){
-	results.removeChild(results.firstChild);  		
-  	}
+    emptyResults();
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode(message));
+    results.appendChild(span);
   }
 
-  function displayResults(domainInfo){
-  	emptyResults();
-  	console.log(domainInfo);
-  	if(domainInfo.length === 0){
-  		displayErrorMessage("Lén er ekki skráð");
-  		return;
-  	}
-  	const [{domain, registered, lastChange,expires, registrantname,email,address,country}] = domainInfo;
+  function loading() {
+    emptyResults();
+    const div = document.createElement('div');
+    div.classList.add('loading');
+    const img = document.createElement('img');
+    img.setAttribute('src', 'loading.gif');
+    div.appendChild(img);
 
-
-  	const dl = document.createElement("dl");
-  	results.appendChild(dl);
-  	addToList(dl,"Lén", domain);
-  	addToList(dl,"Skráð", new Date(registered).toISOString().split('T')[0]);
-  	addToList(dl,"Síðast breytt",  new Date(lastChange).toISOString().split('T')[0]);
-  	addToList(dl,"Rennur út",  new Date(expires).toISOString().split('T')[0]);
-  	addToList(dl,"Skráningaraðili", registrantname);
-  	addToList(dl,"Netfang", email);
-  	addToList(dl,"Heimilisfang", address);
-  	addToList(dl,"Land", country);
-
+    const text = document.createElement('span');
+    text.appendChild(document.createTextNode('Leita af Léni'));
+    div.appendChild(text);
+    results.appendChild(div);
   }
 
-  function loading(){
-  	emptyResults();
-  	let div = document.createElement("div");
-  	div.classList.add("loading");
-  	let img = document.createElement("img");
-  	img.setAttribute("src","loading.gif");
-  	div.appendChild(img);
 
-  	let text = document.createElement("span");
-  	text.appendChild(document.createTextNode("Leita af Léni"));
-  	div.appendChild(text);
-  	results.appendChild(div);
+  function addToList(listparent, title, data) {
+    if (data !== '') {
+      const datatitle = document.createElement('dt');
+      datatitle.appendChild(document.createTextNode(title));
+      const datatext = document.createElement('dd');
+      datatext.appendChild(document.createTextNode(data));
+      listparent.appendChild(datatitle);
+      listparent.appendChild(datatext);
+    }
   }
 
-  function addToList(listparent, title, data){
-  	if(data != ""){
-	  	const datatitle = document.createElement("dt");
-	  	datatitle.appendChild(document.createTextNode(title));
-	  	const datatext = document.createElement("dd");
-	  	datatext.appendChild(document.createTextNode(data));
-	  	listparent.appendChild(datatitle);
-	  	listparent.appendChild(datatext);
-  	}
+  function displayResults(domainInfo) {
+    emptyResults();
+    if (domainInfo.length === 0) {
+      displayErrorMessage('Lén er ekki skráð');
+      return;
+    }
+    const [{ domain, registered, lastChange, expires, registrantname, email, address, country }] = domainInfo; /* eslint-disable-line */
+    const dl = document.createElement('dl');
+    results.appendChild(dl);
+    addToList(dl, 'Lén', domain);
+    addToList(dl, 'Skráð', new Date(registered).toISOString().split('T')[0]);
+    addToList(dl, 'Síðast breytt', new Date(lastChange).toISOString().split('T')[0]);
+    addToList(dl, 'Rennur út', new Date(expires).toISOString().split('T')[0]);
+    addToList(dl, 'Skráningaraðili', registrantname);
+    addToList(dl, 'Netfang', email);
+    addToList(dl, 'Heimilisfang', address);
+    addToList(dl, 'Land', country);
+  }
+  function fetcher(dom) {
+    loading();
+    fetch(`${API_URL}${dom}`)
+      .then((result) => {
+        if (!result.ok) {
+          return displayErrorMessage('Villa í tengingu');
+        }
+        return result.json();
+      })
+      .then((data) => {
+        displayResults(data.results);
+      })
+      .catch((error) => {
+        displayErrorMessage('Villa við að sækja gögn');
+        error.log(error);
+      });
+  }
+
+  function formHandler(e) {
+    e.preventDefault();
+    const domain = document.querySelector('input').value;
+    if (domain.trim() === '') {
+      displayErrorMessage('Lén verður að vera strengur');
+      document.querySelector('input').value = '';
+      return;
+    }
+    fetcher(domain);
   }
 
   function init(_form) {
-    form = _form;
     _form.addEventListener('submit', formHandler);
   }
-  
+
   return {
     init,
   };
